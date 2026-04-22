c2a0b42f4078a573896ee9b1c87d9b63760b3b51
Xian Xu <xianxu@gmail.com>
2026-04-22 11:10:55 -0700

    color of zellij

--- .openshell/dotfiles/zellij/clock.sh
+++ .openshell/dotfiles/zellij/clock.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Match zellij status-bar background (gruvbox black: 60,56,54)
+BG="\033[48;2;60;56;54m"
+FG="\033[38;2;213;196;161m"
+RST="\033[0m"
 while true; do
-    printf '\r%s' "$(date +'%H:%M:%S')"
+    cols=$(tput cols 2>/dev/null || echo 10)
+    time_str=$(date +'%H:%M:%S')
+    padding=$((cols - ${#time_str}))
+    printf "\r${BG}${FG}%*s%s${RST}" "$padding" "" "$time_str"
     sleep 1
 done
